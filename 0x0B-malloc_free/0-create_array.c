95e1c3badf605f3562adb5ba6e0c0d7dcc876669
Iamshe1002 <kariukimary2001@gmail.com>
2023-08-08 14:58:05 -0700

    task 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,22 +1,26 @@
-#include"holberton.h"
+#include"main.h"
 #include<stdlib.h>
 
 /**
- * *create_array - creates an array of chars, and initializes
+ * create_array - creates an array of chars, and initializes
  * it with a specific char.
  * @c: char to initialize
  * @size: number of bytes to allocate
  *
  * Return: a pointer to the array or null if it falls
+ *
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *array = malloc(size):
+	char *str;
+	unsigned int i;
 
-	if (size == 0 || array == 0)
-	       return (NULL):
-	while (size --)
-		array[size] = c:
-	return (array):
+	str = malloc(sizeof(char) * size);
+        if (size == 0 || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		str(i) = c;
+	return (str);
 }
